7fd58f4636a24d0d1a66b43ec0e1e6b0dd2f4590
TwilCynder <twilcynder@gmail.com>
2019-06-06 06:29:54 +0200

    Images, application draw loop, and other stuff

--- goomba.js
+++ goomba.js
@@ -1,10 +1,10 @@
-gmb = {}
+var gmb = {}
 
 gmb.draw = function(element, context){
-    context = context || element.renderContext || (element.game && element.game.renderContext) || gmb.defaultContext;
+    context = context || element.context || (element.application && element.application.context) || gmb.defaultContext;
 
     if (typeof element.draw == "function"){
-        element.draw();
+        element.draw(context);
     }
 }
 
@@ -17,26 +17,90 @@ gmb.GUILayer = class{
     getElements(){
         return this.elements;
     }
+
+    draw(context){
+        for (let i = 0; i < this.elements.length; i++){
+            gmb.draw(this.elements[i], context)
+        }
+    }
+
+    add(element){
+        return this.elements.splice(0, 0, element);
+    }
+
+    sendElementToPosition(element, sendIndex){
+        let index = null
+        if (typeof element != "number"){
+            for (let i = 0; i < this.elements.length; i++){
+                if (this.elements[i] == element){
+                    index = i;
+                }
+            }
+            if (index === null){
+                return false
+            }
+        } else {
+            index = element;
+            if (index >= this.elements.length || sendIndex >= this.elements.length){
+                return false;
+            }
+        }
+
+        element = this.elements[i];
+        this.elements.splice(index, 1)
+        this.elements.splice(sendIndex, 0, element)
+        return sendIndex;
+    }
+
+    sendElementToBottom(element){
+        sendElementToPosition(element, 0)
+    }
+
+}
+
+let loopCallback = function(app){
+    app.draw()
 }
 
 gmb.Application = class{
 
-    constructor(context){
-        this.context = context;
+    constructor(a, b, c){
+        this.isApplication = true;
+        
+        if (typeof a == "number" && typeof b == "number"){
+            let canvas = document.createElement("canvas");
+            canvas.width = a;
+            canvas.height = b;
+            this.context = canvas.getContext("2d");
+            if (!c) document.body.appendChild(canvas);
+        } else if (typeof a == "object"){
+            this.context = a;
+        }
+
         this.gui = [];
+        this.config = {
+            strictLayers: false,
+            clearColor: "#000000"
+        };
+    }
+
+    start(){
+        setInterval(loopCallback, 16, this)
     }
 
     addGUILayer(name, position = -1){
-        assert(typeof name == "string");
+        assert(typeof name == "string", "Bad argument #1 to addGUILayer : name must be a string.");
         position = parseInt(position);
 
-        let layer = new gmb.GUILayer(name)
+        let layer = new gmb.GUILayer(name);
 
         if (position = -1) {
-            this.push(layer)
+            this.gui.push(layer);
+            return layer;
         }
 
         this.gui.splice(position, 0, layer);
+        return layer;
     }
 
     delGUILayer(position){
@@ -56,10 +120,115 @@ gmb.Application = class{
     draw(context){
         context = context || this.context;
 
+        if (!context) return false;
+
+        if (this.config.clearColor != "none" && context.fillRect){
+            context.fillStyle = this.config.clearColor;
+            context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        }
+
+        for (let i = 0; i < this.gui.length; i++){
+            this.gui[i].draw(context);
+        }
     }
+
+    getContext(){
+        return this.context;
+    }
+
+    setContext(context){
+        this.context = context
+    }
+
+    setStrictGUILayers(mode){
+        this.config.strictLayers = mode;
+    }
+
+    setClearColor(color){
+        if (typeof color == "number") {
+            if (color < 0) {
+                color = "none"
+            } else {
+            color = gmb.cssColor(color);
+            }
+        }
+        this.config.clearColor = color;
+    }
+
 }
 
 function assert(condition, error){
-    if (!(typeof error == "string")) throw "Bad argument #1 to assert : error must be a string";
+    if (!(typeof error == "string")) throw "Bad argument #2 to assert : error must be a string";
     if (!condition) throw error;
+}
+
+gmb.cssColor = function(color){
+    assert(typeof color == "number", "Bad argument #1 to cssColor : color must be a number.");
+    return '#' + color.toString(16).padStart(6, "0")
+}
+
+gmb.loadImage = function(application, url, callback){
+    assert(typeof url == "string", "Bad argument #2 to loadImage : url must be a string");
+
+    let img = new Image();
+    img.onload = function(event){
+        let img = event.target
+        if (typeof callback == "function") callback(img);
+        if (application && typeof application.onload == "function"){
+            application.onload(img);
+        } 
+        img.loaded = true;
+    };
+    img.src = url;
+    img.loaded = false;
+    
+    return img;
+}
+
+gmb.voidFunction = function(){};
+
+gmb.GUIElement = class{
+    constructor(application, layerName, x, y){
+        this.isGMBGUIElement = true;
+
+        if (application && application.isApplication && typeof layerName == "string") {
+            let layer = application.getGUILayer(layerName, 1)
+            if ( layer == null){
+                if (application.config.strictLayers){
+                    throw "The specified GUI layer doesn't exist"
+                } else {
+                    layer = application.addGUILayer(layerName);
+                }
+            }
+
+            layer.add(this)
+        }
+
+        this.x = x;
+        this.y = y;
+        this.application = application;
+    }
+}
+
+
+gmb.StaticImage = class extends gmb.GUIElement {
+    constructor(application, layerName, x, y, img, w, h){
+        if (!img) {
+            this.img = {loaded: false}
+            return false;
+        }
+        if (typeof img == "string"){
+            img = gmb.loadImage(application, img);
+        }
+        super(application, layerName, x, y);
+        this.w = w;
+        this.h = h;
+        this.img = img;
+    }
+
+    draw(context){
+        if (typeof context.drawImage == "function" && this.img.loaded){
+            context.drawImage(this.img, this.x, this.y);
+        }
+    }
 }
